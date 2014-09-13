df57e053b8203947bba3e00192e8c2c48fc8a935
Jain <038518@USHOM00MB038518.local>
2014-09-13 15:25:45 -0400

    Initial commit

---
+++ AddToDoItem.swift
@@ -0,0 +1,43 @@
+//
+//  AddToDoItem.swift
+//  ToDoList
+//
+//  Created by Jain, Vaibhav on 9/12/14.
+//  Copyright (c) 2014 Jain, Vaibhav. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoItem: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("TODO view is loaded now.")
+        
+        var tdi = ToDoItem(name: "Wash Car")
+        
+        println("my first todo item = \(tdi.toString())")
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+   
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Jain, Vaibhav on 9/13/14.
+//  Copyright (c) 2014 Jain, Vaibhav. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+    
+    var itemName: String
+    var isDone: Bool
+    let creationDate: NSDate
+    
+    init (name: String) {
+        itemName = name;
+        isDone = false
+        creationDate = NSDate()
+    }
+    
+    func toString() -> String{
+        
+        var dateformatter = NSDateFormatter()
+        
+        return "TODO Object --> \(self.itemName), \(self.isDone), \(self.creationDate.descriptionWithLocale(NSLocale.currentLocale()))"
+    }
+   
+}
