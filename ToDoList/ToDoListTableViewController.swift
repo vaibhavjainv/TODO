37147008fb97698e529c683e4a4e04bed06f1483
Jain <038518@USHOM00MB038518.local>
2014-09-14 00:51:04 -0400

    Added delete functionality

--- ToDoList/ToDoListTableViewController.swift
+++ ToDoList/ToDoListTableViewController.swift
@@ -113,17 +113,26 @@ class ToDoListTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        
+        println("POKA")
+        
         if editingStyle == .Delete {
+            
+            println("KAMPAI")
+           
             // Delete the row from the data source
+            todoLists.removeAtIndex(indexPath.row)
+            
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+//            else if editingStyle == .Insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
